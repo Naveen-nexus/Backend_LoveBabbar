8f2219a35535954a4f103921715cc91b3e4955bf
nk4451@srmist.edu.in <nk4451@srmist.edu.in>
2025-11-11 01:18:17 +0530

    Initial commit: Express server with GET and POST endpoints

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+//server instantiate
+
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
